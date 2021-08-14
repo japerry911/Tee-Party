4ee19d6092956a380c74eb96d4151fb23563f526
Jack Perry <jack.e.perry@outlook.com>
2021-08-14 14:56:26 -0600

    📦 NEW: foundation set

---
+++ frontend/src/components/HeroHeader/HeroHeader.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { useStyles } from './HeroHeaderStyles';
+
+const HeroHeader = () => {
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <h1>HERO HEADER</h1>
+    </Fragment>
+  );
+};
+
+export default HeroHeader;
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import { useStyles } from './HomeStyles';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
 
   return (
     <div className={classes.mainDivStyle}>
+      <HeroHeader />
       <h1>Home Page</h1>
     </div>
   );
