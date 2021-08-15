62c33b5f0717ee4f710ff05ee11ef240d263945b
Jack Perry <jack.e.perry@outlook.com>
2021-08-15 12:37:56 -0600

    👌 IMPROVE: top 10 cards list added, ready to merge

---
+++ frontend/src/misc/slides.js
@@ -0,0 +1,54 @@
+export const californiaSlides = [
+  {
+    name: '1. Cypress Point',
+    imgUrl:
+      'https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/california/01.jpg',
+    description: 'Pebble Beach',
+  },
+  {
+    name: '2. Pebble Beach G. Links',
+    imgUrl:
+      'https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/california/02.jpg',
+    description: 'Pebble Beach',
+  },
+  {
+    name: '3. Los Angeles C.C. (North)',
+    imgUrl:
+      'https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/california/03.jpg',
+    description: 'Los Angeles',
+  },
+  {
+    name: '4. Riviera C.C.',
+    imgUrl:
+      'https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/california/04.jpg',
+    description: 'Pacific Palisades',
+  },
+  {
+    name: '5. The Olympic Club (Lakes)',
+    imgUrl:
+      'https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/california/05.jpg',
+    description: 'San Francisco',
+  },
+  {
+    name: '6. San Francisco G.C.',
+    imgUrl:
+      'https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/california/06.jpg',
+    description: 'San Francisco',
+  },
+  { name: '7. Spyglass Hill G.C.', imgUrl: '', description: 'Pebble Beach' },
+  {
+    name: '8. Monterey Peninsula C.C (Shore)',
+    imgUrl: '',
+    description: 'Pebble Beach',
+  },
+  {
+    name: '9. Monterey Peninsula (Dunes)',
+    imgUrl: '',
+    description: 'Pebble Beach',
+  },
+  {
+    name: '10. The Valley C. of Montecito',
+    imgUrl: '',
+    description: 'Montecito',
+  },
+];
--- frontend/src/pages/CaliforniaGolfCourses/CaliforniaGolfCourses.js
+++ frontend/src/pages/CaliforniaGolfCourses/CaliforniaGolfCourses.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import HeroHeader from '../../components/HeroHeader/HeroHeader';
+import ImageCard from '../../components/ImageCard/ImageCard';
+import useTheme from '@material-ui/core/styles/useTheme';
+import { californiaSlides } from '../../misc/slides';
 import { useStyles } from './CaliforniaGolfCoursesStyles';
 
 const CaliforniaGolfCourses = () => {
   const classes = useStyles();
+  const theme = useTheme();
 
   return (
     <Grid
@@ -21,21 +25,35 @@ const CaliforniaGolfCourses = () => {
       />
       <Grid
         item
-        container
-        direction='column'
         className={classes.gridContainerMidHeightStyle}
-        style={{ marginTop: '5rem' }}
+        style={{ marginTop: '5rem', minHeight: '5rem' }}
       >
-        <Grid item>
-          <Typography
-            className={classes.headerTextStyle}
-            variant='h5'
-            gutterBottom
-            align='center'
-          >
-            Top 10 Golf Courses in California
-          </Typography>
-        </Grid>
+        <Typography
+          className={classes.headerTextStyle}
+          variant='h3'
+          gutterBottom
+          align='center'
+        >
+          Top 10 Golf Courses in California
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        justifyContent='space-evenly'
+        className={classes.gridContainerMidHeightStyle}
+        style={{ backgroundColor: theme.colors.sandColor, padding: '1rem' }}
+      >
+        {californiaSlides.map((slide, idx) => (
+          <ImageCard
+            key={`ca-${idx}`}
+            imgUrl={slide.imgUrl}
+            headerText={slide.name}
+            btnText='Learn More'
+            widthAdj='20%'
+            marginAdj='1rem'
+          />
+        ))}
       </Grid>
     </Grid>
   );
