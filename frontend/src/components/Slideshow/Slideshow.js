fb49a91b5ba4b4d2d7886984e579d94dbbfa6994
Jack Perry <jack.e.perry@outlook.com>
2021-08-15 11:44:35 -0600

    📦 NEW: slideshow foundation built

---
+++ frontend/src/components/Slideshow/Slideshow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { useStyles } from './SlideshowStyles';
+
+const Slideshow = (props) => {
+  const classes = useStyles();
+
+  return null;
+};
+
+export default Slideshow;
--- frontend/src/pages/CaliforniaGolfCourses/CaliforniaGolfCourses.js
+++ frontend/src/pages/CaliforniaGolfCourses/CaliforniaGolfCourses.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import { useStyles } from './CaliforniaGolfCoursesStyles';
 
 const CaliforniaGolfCourses = () => {
   const classes = useStyles();
 
-  return null;
+  return (
+    <Grid
+      container
+      direction='column'
+      className={classes.mainDivStyle}
+      alignItems='center'
+    >
+      <HeroHeader
+        imgUrl='https://tee-party-1243912491248032048023.s3.us-east-2.amazonaws.com/golf-courses/Hero+Image_Golf+Courses+Page.jpg'
+        headerText='Best Golf Courses in California'
+        btnText='Search Courses'
+      />
+      <Grid
+        item
+        container
+        direction='column'
+        className={classes.gridContainerMidHeightStyle}
+        style={{ marginTop: '5rem' }}
+      >
+        <Grid item>
+          <Typography
+            className={classes.headerTextStyle}
+            variant='h5'
+            gutterBottom
+            align='center'
+          >
+            Top 10 Golf Courses in California
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CaliforniaGolfCourses;
