5b9e8cfdb8a678ba55ab109a4abd6d4bdf684e29
Jack Perry <jack.e.perry@outlook.com>
2021-08-15 07:32:54 -0600

    👌 IMPROVE: heroheader added to golf-courses page

--- frontend/src/pages/GolfCourses/GolfCoursesStyles.js
+++ frontend/src/pages/GolfCourses/GolfCoursesStyles.js
@@ -1,3 +1,12 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-export const useStyles = makeStyles((theme) => ({}));
+export const useStyles = makeStyles((theme) => ({
+  mainDivStyle: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 0,
+    margin: 0,
+    width: '100%',
+  },
+}));
