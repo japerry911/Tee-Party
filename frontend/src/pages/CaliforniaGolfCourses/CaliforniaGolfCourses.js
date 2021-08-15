8a6fc78465eb8f5cfd80dc150fb2b0d50dad969a
Jack Perry <jack.e.perry@outlook.com>
2021-08-15 11:26:54 -0600

    📦 NEW: base foundation built

---
+++ frontend/src/pages/CaliforniaGolfCourses/CaliforniaGolfCourses.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useStyles } from './CaliforniaGolfCoursesStyles';
+
+const CaliforniaGolfCourses = () => {
+  const classes = useStyles();
+
+  return null;
+};
+
+export default CaliforniaGolfCourses;
