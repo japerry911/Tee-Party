0df5fa98c14b9b9a6670675c663bc5e07d3cc95a
Jack Perry <jack.e.perry@outlook.com>
2021-08-16 07:33:37 -0600

    👌 IMPROVE: topics and posts seeds improved

--- backend/seeders/20210815235522-Topics.js
+++ backend/seeders/20210815235522-Topics.js
@@ -4,7 +4,26 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Topics', [
       {
+        id: 1,
         name: 'Hole in Ones',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        name: 'Favorite Courses',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 3,
+        name: 'Favorite Clubs',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 4,
+        name: 'Skylord Tournament News',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
